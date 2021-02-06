60c8a01369d5d1c47cd5151f3924df707d2d7d92
Tomomi <58757939+tomi-design@users.noreply.github.com>
2021-02-06 02:57:08 -0500

    Added styles to the p tag where the message will display, and added comments to explain what's happening in JS functions

--- script.js
+++ script.js
@@ -3,17 +3,29 @@ const text = document.getElementById('text');
 const sendButton = document.getElementById('send');
 const clearButton = document.getElementById('clear');
 
+
+//This function will display the message at the bottom of the page in the <p> element
 function display(){
+
+    //This textValue will grab the value of the input type with the id of "text" 
     let textValue = text.value;
+    //The content of the <p> tag with the id of "message" will be the value that textValue holds.
     message.innerHTML = textValue;
+
 }
 
 sendButton.addEventListener('click', display);
 
 
+//This function will clear the messages in the message section and in the input field
 function clearMessage() {
-  message.innerHTML = ' ';
-  text.value = ' ';
+
+    //This says to make content of the variable "message" empty
+    message.innerHTML = ' ';
+    //This says to make the content of the variable text empty
+    text.value = ' ';
+
 }
 
 clearButton.addEventListener('click', clearMessage);
+
