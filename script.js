ea72f3f351ff8c4c6290056fcd11e37fcb0c4e80
Tomomi <58757939+tomi-design@users.noreply.github.com>
2021-02-08 01:31:09 -0500

    Modified event handler to have anonymous functions

--- script.js
+++ script.js
@@ -4,28 +4,25 @@ const sendButton = document.getElementById('send');
 const clearButton = document.getElementById('clear');
 
 
+
 //This function will display the message at the bottom of the page in the <p> element
-function display(){
+sendButton.addEventListener('click', function() {
 
     //This textValue will grab the value of the input type with the id of "text" 
     let textValue = text.value;
     //The content of the <p> tag with the id of "message" will be the value that textValue holds.
     message.innerHTML = textValue;
 
-}
-
-sendButton.addEventListener('click', display);
+});
 
 
 //This function will clear the messages in the message section and in the input field
-function clearMessage() {
+clearButton.addEventListener('click', function() {
 
     //This says to make content of the variable "message" empty
     message.innerHTML = ' ';
-    //This says to make the content of the variable text empty
+    //This says to make the content of the variable "text" empty
     text.value = ' ';
 
-}
-
-clearButton.addEventListener('click', clearMessage);
+});
 
